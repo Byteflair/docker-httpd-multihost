6e31a681fbd28d828306cf1e3e775ef8f4d0cd57
Daniel Cerecedo <daniel.cerecedo@byteflair.com>
2016-06-13 19:39:37 +0200

    added rever proxy sample

--- Dockerfile
+++ Dockerfile
@@ -15,7 +15,7 @@ RUN rm -Rf /var/www/html \
  && rm -f /apache2/sites-enabled/000-default.conf \
  && rm -f /apache2/sites-available/000-default.conf
 
-#Copy configuration and content for deault site, foo.com (http sample) and bar.com (https sample)
+#Copy configuration and content for deault site, foo.com (http sample), bar.com (https sample) and real-foo
 ADD apache2 /etc/apache2
 ADD www /var/www
 
@@ -23,15 +23,14 @@ RUN chmod -R 755 /var/www
 
 
 #Enable SSL support and create a selfsigned certificate for bar.com
-RUN a2enmod ssl \
+RUN a2enmod proxy proxy_http proxy_html substitute deflate xml2enc rewrite ssl \
  && mkdir /etc/apache2/ssl \
  && mkdir /etc/apache2/ssl/bar \
  && openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/apache2/ssl/bar/bar.key -out /etc/apache2/ssl/bar/bar.crt -subj "/C=ES/ST=Madrid/L=Madrid/O=Byteflair/OU=Docker Team/CN=bar.com"
+
 
 EXPOSE 80 443
 
 VOLUME ["/var/www", "/var/log/apache2", "/etc/apache2"]
 
-CMD ["-d /etc/apache2"]
-
-ENTRYPOINT ["apache2ctl","-DFOREGROUND"]
+ENTRYPOINT ["apache2ctl","-DFOREGROUND","-d /etc/apache2","-e INFO"]
--- rebuild.sh
+++ rebuild.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#Accepts a --no-cache argument to build image from scratch
+#Accepts the name of the container (mandatory)  and --no-cache argument (optional) to build image from scratch
 #Stop and clean
-( docker stop apache )
-( docker rm -v apache )
+( docker stop $1 )
+( docker rm -v $1 )
 #Build and run
-( docker build $1 -t byteflair/httpd-multihost . \
-&& docker run --name apache -p 80:80 -p 443:443 -v /etc/apache2 -v /var/www -v /var/log/apache2 -tid byteflair/httpd-multihost )
+( docker build $2 -t byteflair/httpd-multihost . \
+&& docker run --name $1 -p 80:80 -p 443:443 -v /etc/apache2 -v /var/www -v /var/log/apache2 -tid byteflair/httpd-multihost )
 #Extract location of volumes
-echo VOL_SERVER_ROOT=$(docker inspect -f '{{range $a := .Mounts}}{{if eq .Destination "/etc/apache2"}}{{.Source}}{{end}}{{end}}' apache)
-echo VOL_DOC_ROOT=$(docker inspect -f '{{range $a := .Mounts}}{{if eq .Destination "/var/www"}}{{.Source}}{{end}}{{end}}' apache)
-echo VOL_LOG=$(docker inspect -f '{{range $a := .Mounts}}{{if eq .Destination "/var/log/apache2"}}{{.Source}}{{end}}{{end}}' apache)
+echo VOL_SERVER_ROOT=$(docker inspect -f '{{range $a := .Mounts}}{{if eq .Destination "/etc/apache2"}}{{.Source}}{{end}}{{end}}' $1)
+echo VOL_DOC_ROOT=$(docker inspect -f '{{range $a := .Mounts}}{{if eq .Destination "/var/www"}}{{.Source}}{{end}}{{end}}' $1)
+echo VOL_LOG=$(docker inspect -f '{{range $a := .Mounts}}{{if eq .Destination "/var/log/apache2"}}{{.Source}}{{end}}{{end}}' $1)
+
